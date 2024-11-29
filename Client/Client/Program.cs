f71534c204a6df0ae9d873e87431e3620090a22c
akkumut <umuak01@eamv.dk>
2024-11-29 23:34:31 +0100

    sad

---
+++ Client/Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 7080);
+                Console.WriteLine($"Connected to server");
+
+                NetworkStream stream = client.GetStream();
+                string message = "Hello, Server!";
+
+                byte[] dataToSend = Encoding.ASCII.GetBytes(message);
+                stream.Write(dataToSend, 0, dataToSend.Length);
+                Console.WriteLine("Data sent!");
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Server response: {response}");
+
+                Console.WriteLine("Client has closed the connection!");
+                client.Close();
+
+                //Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught in client: {ex.Message} - ST: {ex.StackTrace}");
+                throw;
+            }
+        }
+    }
+}
---
+++ Client/serverexample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            TcpListener server = new TcpListener(IPAddress.Any, 7080);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server has started!");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("New client connected!");
+                    Thread clientThread = new Thread(() => handleClient(client));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught in server: {ex.Message} - ST: {ex.StackTrace}");
+            }
+        }
+
+        static void handleClient(TcpClient client)
+        {
+            #region Get client request
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int bytesRead = stream.Read(buffer, 0, client.ReceiveBufferSize);
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received: {dataReceived}");
+            #endregion
+
+            #region Simulated stress
+            Console.WriteLine("Simulating stress...");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            #endregion
+
+            #region Reply to the request
+            byte[] dataToSend = Encoding.ASCII.GetBytes($"Hello, Client! you said {dataReceived}");
+            stream.Write(dataToSend, 0, dataToSend.Length);
+            Console.WriteLine("Data sent!");
+            #endregion
+
+            #region Close connection
+            client.Close();
+            Console.WriteLine("Client disconnected!");
+            #endregion
+        }
+    }
+}
